da214b39ec2bd00edc30b1a7b70c5e00ee043b42
lenoo <lenoo@chiliveri>
2021-07-16 19:52:11 +0530

    Project 1st commit

---
+++ MyOwnProjects/src/main/java/Package1/Test1.java
@@ -0,0 +1,58 @@
+package Package1;
+
+public class Test1 implements TestINt1{
+	
+            Test1() {
+		
+            	System.out.println("constructor code");
+		// TODO Auto-generated method stub
+	
+		
+	}
+            
+            public void open() {
+        		// TODO Auto-generated method stub
+            	
+            	System.out.println("open word");
+        		
+        	}
+
+	public void update() {
+		// TODO Auto-generated method stub
+		System.out.println("update word");
+		
+	}
+
+	public void close() {
+		// TODO Auto-generated method stub
+		System.out.println("close word");
+	}
+		
+	
+	
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+           
+	
+
+	
+	TestINt1 obj = new Test1();
+	//obj.open();
+	//obj.update();
+	//obj.close();
+	
+	//System.out.println("hhdbh");
+	}
+
+	
+}
+	
+	
+	
+
+	
+		
+	
+
+
---
+++ MyOwnProjects/src/main/java/Package1/interviewprep.java
@@ -0,0 +1,63 @@
+package Package1;
+
+import java.util.Scanner;
+
+public class interviewprep {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		//to print reverse of an array
+	/*to reverse an array when values are declared preiorly
+		int[] arr= {1,2,3,4,5};
+	for(int i=4; i>=0; i--)
+	{
+		System.out.println(arr[i]);
+
+	}*/
+		/*int j=0; 
+		int[] arr = new int[40];
+		int[] rev = new int[40];
+		System.out.println("enter the size");
+		Scanner sc = new Scanner(System.in);
+		int size = sc.nextInt();
+		System.out.println("enter the no");
+		for(int i=0; i<size; i++) {   
+			arr[i] = sc.nextInt();			
+	}                                       
+		                                        
+		for(int i=size-1;i>=0;i--) {
+			rev[j]=arr[i];  
+			j++;
+			
+		}
+		
+		for(int i=0; i<size;i++) {
+			System.out.println(rev[i]);
+		}*/
+		
+		String a = "ant";
+		String r = "";
+		for(int i=a.length()-1;i>=0;i--)
+		{
+			r.le
+			
+			
+	
+		  //}
+		//System.out.println(a.length());
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+
+}}
